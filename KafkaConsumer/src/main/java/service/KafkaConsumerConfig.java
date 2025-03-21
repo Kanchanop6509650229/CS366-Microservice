af3f09e840adc89bb58ebd145c72a40ec0a305a0
Kanchanop6509650229 <kanchanop.bua@dome.tu.ac.th>
2025-03-21 19:57:49 +0700

    Refactor Kafka consumer configuration to use ErrorHandlingDeserializer for improved error handling during message deserialization

--- KafkaConsumer/src/main/java/service/KafkaConsumerConfig.java
+++ KafkaConsumer/src/main/java/service/KafkaConsumerConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import service.model.Models;
@@ -24,18 +25,16 @@ public class KafkaConsumerConfig {
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "my-group");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
+        props.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class);
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-
-        JsonDeserializer<Models> deserializer = new JsonDeserializer<>(Models.class);
-        deserializer.setRemoveTypeHeaders(true); // แก้ปัญหาการ Deserialize
-        deserializer.addTrustedPackages("com.example.kafka", "service");
-        //deserializer.setUseTypeMapperForKey(true);
-
-        return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), deserializer);
+        props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
+        props.put(JsonDeserializer.TYPE_MAPPINGS, "com.example.kafkaproducerapp.model.Models:service.model.Models");
+        props.put(JsonDeserializer.VALUE_DEFAULT_TYPE, "service.model.Models");
+        
+        return new DefaultKafkaConsumerFactory<>(props);
     }
 	
-
     //KafkaListenerContainerFactory for JSON
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, Models> modelKafkaListenerContainerFactory() {
