8eca23f7a5d3745e2839693e49fb6178b5400b12
Kanchanop6509650229 <kanchanop.bua@dome.tu.ac.th>
2025-03-21 20:04:40 +0700

    Enhance KafkaConsumerApplicationTests with specific Spring Boot configuration

--- KafkaConsumer/src/test/java/service/KafkaConsumerApplicationTests.java
+++ KafkaConsumer/src/test/java/service/KafkaConsumerApplicationTests.java
@@ -1,13 +1,15 @@
 package service;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = KafkaConsumerApplication.class)
+@EnableAutoConfiguration(exclude = KafkaAutoConfiguration.class)
 class KafkaConsumerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
