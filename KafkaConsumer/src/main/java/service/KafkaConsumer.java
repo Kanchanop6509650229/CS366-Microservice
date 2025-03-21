c84626772700788a240f99f33efc18b8c30a22c1
Kanchanop6509650229 <kanchanop.bua@dome.tu.ac.th>
2025-03-21 19:46:43 +0700

    Add Kafka consumer for JSON messages and model class implementation

--- KafkaConsumer/src/main/java/service/KafkaConsumer.java
+++ KafkaConsumer/src/main/java/service/KafkaConsumer.java
@@ -3,12 +3,19 @@ package service;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+import service.model.Models;
+
 @Component
 public class KafkaConsumer {
-	@KafkaListener(topics = "test-topic", groupId = "my-group")
 
+	@KafkaListener(topics = "test-topic", groupId = "my-group")
 	public void consume(String message) {
 		// Print statement
 		System.out.println("message = " + message);
+	}
+
+	@KafkaListener(topics = "models-topic", groupId = "json-group", containerFactory = "modelKafkaListenerContainerFactory")
+	public void consumeJsonMessage(Models model) {
+		System.out.println("Received JSON Message: " + model);
 	}
 }
--- KafkaConsumer/src/main/java/service/KafkaConsumerConfig.java
+++ KafkaConsumer/src/main/java/service/KafkaConsumerConfig.java
@@ -4,42 +4,46 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 
-//import com.fasterxml.jackson.databind.deser.std.StringDeserializer;
+import service.model.Models;
 
-@EnableKafka
+
+//@EnableKafka
 @Configuration  
 public class KafkaConsumerConfig {
-	@Bean
-    public ConsumerFactory<String, String> consumerFactory() { 
-  
-        // Creating a Map of string-object pairs 
-        Map<String, Object> config = new HashMap<String, Object>(); 
-  
-        // Adding the Configuration 
-        //config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "[::1]:9092"); 
-        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092"); 
-        config.put(ConsumerConfig.GROUP_ID_CONFIG, "my-group"); 
-        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class); 
-        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class); 
-  
-        return new DefaultKafkaConsumerFactory<>(config); 
-    } 
+    @Bean
+    public ConsumerFactory<String, Models> modelConsumerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "my-group");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        JsonDeserializer<Models> deserializer = new JsonDeserializer<>(Models.class);
+        deserializer.setRemoveTypeHeaders(true); // แก้ปัญหาการ Deserialize
+        deserializer.addTrustedPackages("com.example.kafka", "service");
+        //deserializer.setUseTypeMapperForKey(true);
+
+        return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), deserializer);
+    }
 	
-	
-	 // Creating a Listener 
-    public ConcurrentKafkaListenerContainerFactory<String, String> 
-    concurrentKafkaListenerContainerFactory() { 
-        ConcurrentKafkaListenerContainerFactory<String, String> factory 
-            = new ConcurrentKafkaListenerContainerFactory<String, String>(); 
-        factory.setConsumerFactory(consumerFactory()); 
-        return factory; 
-    } 
+
+    //KafkaListenerContainerFactory for JSON
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, Models> modelKafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, Models> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(modelConsumerFactory());
+        return factory;
+    }
+    
+    
 }
